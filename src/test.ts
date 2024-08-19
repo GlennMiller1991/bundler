ec09d79183aaa571fad2eebb7e847028584e0dc4
AlexB <gatesoftommorow91@mail.ru>
2024-08-19 20:42:18 +0400

    jest config __dirname

--- src/test.ts
+++ src/test.ts
@@ -4,5 +4,5 @@ import { run } from 'jest'
 import path from 'path'
 
 run([
-    `--config=${path.resolve(process.env.INIT_CWD, './ jest.config.js')}`
+    `--config=${path.resolve(__dirname, '../jest.config.js')}`
 ])
