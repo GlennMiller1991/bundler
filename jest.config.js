2ebd3bff86626025163069c208140a2486b4f0f4
AlexB <gatesoftommorow91@mail.ru>
2024-08-19 21:10:22 +0400

    bug

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 const path = require("path")
 
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.export = {
+module.exports = {
 	verbose: true,
 	rootDir: path.resolve(process.env.INIT_CWD || process.cwd(), "__tests__"),
 	testEnvironment: "node",
